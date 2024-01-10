21c585e3df32b5b5a71af13189287bc200fa137f
kestulka <kgirdziunas0@gmail.com>
2024-01-10 13:12:08 +0200

    miau

--- script.js
+++ script.js
@@ -1,58 +1,48 @@
-const container = document.getElementById("container")
+const API = "Y7312dlg4bY9Bn0yoW7QbLQ0QLRpP9tz1xmXr1wst38"
 
-const button = document.createElement("button")
-button.innerText = "get country"
-button.style.height = "50px"
-container.appendChild(button)
-
-const input = document.createElement("input")
-input.type = "text"
-input.placeholder = "search for country"
-
-container.append(input,button)
-
-
-const getCountry = async(event) => {
-    event.preventDefault()
-
-    const inputValue = input.value.trim()
-    console.log(`you typed: ${inputValue}`)
-
-    const result = await fetch(`https://restcountries.com/v3.1/name/${inputValue}`)
-    console.log(result)
-
-    const data = await result.json()
-    console.log(data)
-
-    if(result.status === 404){
-        const infoElement = document.createElement("h1")
-        infoElement.innerText = result.statusText
-        container.appendChild(infoElement)
-    } else{
-        const allCards = document.querySelectorAll(".card")
-        allCards.forEach(card=> card.remove())
-
-
-        console.log(data)
-        data.map((country, index)=>{
-            const card = document.createElement("div")
-            card.className = "card"
-
-            const flagImage = document.createElement("img")
-            flagImage.style.border = "thick solid black"
-            flagImage.src = country.flags.png
-            flagImage.alt = country.flags.alt || country.name.common
-
-            const flagTitle = document.createElement("h3")
-            flagTitle.innerText = country.name.common
-
-            card.append(flagImage, flagTitle)
-            container.appendChild(card)
-        })
-    }
-}
-
-button.addEventListener("click", getCountry)
-
-
-
+const container = document.getElementById("container");
+const button = document.createElement("button");
+button.innerText = "AAAAAAAAA";
+ 
+let input = document.createElement("input");
+input.type = "text";
+input.placeholder = "miau";
+ 
+container.append(button, input);
+ 
+const getPhoto = async (event) => {
+  event.preventDefault();
+  const inputValue = input.value.trim();
+  // const result = await fetch(`https://api.unsplash.com/photos/random/?client_id=${API}&count=3`);
+ 
+  const result = await fetch(
+    `https://api.unsplash.com/search/photos/?client_id=${API}&query=${inputValue}&per_page=3`
+  );
+  console.log(result);
+ 
+  const data = await result.json();
+  console.log(data);
+ 
+  if (result.total === 0) {
+    const infoElement = document.createElement("h2");
+    infoElement.innerText = "data not found";
+    container.appendChild(infoElement);
+  } else {
+    const allImages = document.querySelectorAll(".foto");
+    allImages.forEach((image) => image.remove());
+ 
+    data.results.map((image) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      container.appendChild(card);
+ 
+      const cardImage = document.createElement("img");
+      cardImage.className = "foto";
+      cardImage.src = image.urls.small;
+ 
+      container.appendChild(cardImage);
+    });
+  }
+};
+button.addEventListener("click", getPhoto);
+ 
